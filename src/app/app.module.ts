a8673eef3028d8991970a8d5d2c3cb8d9e2634a6
pooja <poojaputtu24@gmail.com>
2018-10-30 01:23:20 +0530

    crud operation with web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { UpdateproductComponent } from './updateproduct/updateproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    UpdateproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,     
+    HttpModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component : HomeComponent},
+      {path: 'product', component : ProductComponent},
+      {path: "updateproduct/:id", component : UpdateproductComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
